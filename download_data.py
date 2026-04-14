e1c51d8c253d9ed3379fe35356ad297ba11f4546
Valentina Njaradi <vnjaradi@hpc-gw2.hpc.swc.ucl.ac.uk>
2026-04-14 15:38:10 +0000

    model trained on 5000 samples

--- download_data.py
+++ download_data.py
@@ -8,5 +8,6 @@ full_train = torchvision.datasets.MNIST("examples/scripts/data/mnist", train=Tru
 full_eval = torchvision.datasets.MNIST("examples/scripts/data/mnist", train=False,  download=True, transform=None)
 train_dataset = full_train.data.reshape(-1, 1, 28, 28)
 eval_dataset = full_eval.data.reshape(-1, 1, 28, 28)
-np.savez('examples/scripts/data/mnist/train_data.npz', data=train_dataset)
+np.savez('examples/scripts/data/mnist/train_data.npz', data=train_dataset[:5000,:,:,:])
 np.savez('examples/scripts/data/mnist/eval_data.npz', data=eval_dataset)
+print(train_dataset[:5000,:,:,:].shape)
